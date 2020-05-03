24006a9472fcc5308c2678897c89a04bbf5145a5
robert <robertdelahoz@yahoo.com>
2020-05-03 14:10:02 -0400

    check

--- waterserver.js
+++ waterserver.js
@@ -11,15 +11,16 @@ var http = require('http').createServer(handler);
 var fs = require('fs'); 
 var io = require('socket.io')(http) 
 
+console.log('starting webserver ');
 
 http.listen(1234); 
 
 function handler (req, res) { 
-  return res.write('hello');
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { 
+  console.log('new request');
+   fs.readFile(__dirname + '/public/index.html', function(err, data) { 
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); 
-      return res.end("404 Not Not Found");
+      return res.end("404 Not Found");
     }
     res.writeHead(200, {'Content-Type': 'text/html'}); 
     res.write(data); 
